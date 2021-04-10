c4e6f8841d28487dc74dd68124708b55f58825b2
Joshua Griffin <griffinjoshua5@gmail.com>
2021-04-10 17:28:07 +1200

    Add update to useToggle api

--- src/shared/hooks/useToggle.ts
+++ src/shared/hooks/useToggle.ts
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 export const useToggle = (initialValue: boolean) => {
-  const [isToggled, setToggledState] = useState(initialValue);
+  const [isOn, setToggledState] = useState(initialValue);
 
   const turnOn = () => setToggledState(true);
   const turnOff = () => setToggledState(false);
   const switchToggle = () => setToggledState((state) => state!);
 
-  return { turnOn, turnOff, switchToggle, isToggled };
+  return { turnOn, turnOff, switchToggle, isOn };
 };
