35bafce7d3891ad737ce04431035cdd93d33823d
Joshua Griffin <griffinjoshua5@gmail.com>
2021-04-10 17:29:59 +1200

    Add only show app after fonts have loaded

--- src/shared/containers/PreFetchContainer.tsx
+++ src/shared/containers/PreFetchContainer.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { useQueryClient } from "react-query";
 import { getAdrSchema } from "../../features/adr/pages/hooks/useADRSchemaQuery";
+import { useHaveFontsLoaded } from "../hooks/useFontLoading";
 import { getPatientSchema } from "../../features/patients/hooks/usePatientSchemaQuery";
 import { getPatientSurveySchema } from "../../features/patients/hooks/usePatientSurveySchemaQuery";
 import { useAuth } from "../hooks";
@@ -8,6 +9,7 @@ import { useAuth } from "../hooks";
 export const PreFetchContainer: FC = ({ children }) => {
   const queryClient = useQueryClient();
   const { username } = useAuth();
+  const fontsHaveLoaded = useHaveFontsLoaded();
 
   if (username) {
     queryClient.prefetchQuery("patientSurveySchema", getPatientSurveySchema);
@@ -15,5 +17,5 @@ export const PreFetchContainer: FC = ({ children }) => {
     queryClient.prefetchQuery("adrSchema", getAdrSchema);
   }
 
-  return <>{children}</>;
+  return <>{fontsHaveLoaded ? children : null}</>;
 };
