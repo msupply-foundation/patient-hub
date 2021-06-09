a5823e721812a0aa1a936c12b9b64bce3e88ddb0
Mark Prins <mark@malcolmprins.net>
2021-06-09 12:57:27 +1200

    add a patient for authenticated users

--- src/features/adr/pages/PatientForm.tsx
+++ src/features/adr/pages/PatientForm.tsx
@@ -1,14 +1,20 @@
 import { FC, useState } from "react";
-import { Paper } from "@material-ui/core";
+import { Button, Container, Paper, Typography } from "@material-ui/core";
 import { PatientDetails, PatientLookup } from "../components";
 
 import { stylesFactory } from "../../../shared/utils";
 import { StepperContainer } from "../../../shared/components/stepper/StepperContainer";
-import { useStep } from "../../../shared/hooks";
+import { useStep, useTranslations } from "../../../shared/hooks";
 import { useAuth } from "../../auth/hooks";
 import { Patient } from "../../patients/types";
 
 const useStyles = stylesFactory({
+  footer: {
+    display: "flex!important",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   img: {
     display: "flex",
     marginLeft: "auto",
@@ -20,6 +26,7 @@ const useStyles = stylesFactory({
     minHeight: "50vh",
     display: "flex",
     alignItems: "flex-start",
+    flexWrap: "wrap",
   },
 });
 
@@ -33,9 +40,11 @@ export const PatientForm: FC<PatientFormProps> = ({ onSubmit, step }) => {
   const { data, setData } = useStep(step);
   const { isGuest } = useAuth();
   const [canContinue, setCanContinue] = useState(false);
+  const [manualEntry, setManualEntry] = useState(isGuest);
+  const { messages } = useTranslations();
 
   const onNextHook = () => canContinue;
-  const patient = data?.patient || {first: "", last: "", date_of_birth: null};
+  const patient = data?.patient || { first: "", last: "", date_of_birth: null };
   const setPatient = (patient: Patient) => setData({ patient });
 
   return (
@@ -46,18 +55,26 @@ export const PatientForm: FC<PatientFormProps> = ({ onSubmit, step }) => {
     >
       <Paper className={classes.paper}>
         <img className={classes.img} alt="logo" src="/patient_hub/logo.png" />
-        {isGuest ? (
+        {manualEntry ? (
           <PatientDetails
             setCanContinue={setCanContinue}
             patient={patient}
             setPatient={setPatient}
           />
         ) : (
-          <PatientLookup
-            setCanContinue={setCanContinue}
-            patient={patient}
-            setPatient={setPatient}
-          />
+          <>
+            <PatientLookup
+              setCanContinue={setCanContinue}
+              patient={patient}
+              setPatient={setPatient}
+            />
+            <Container className={classes.footer}>
+              <Typography mr={2}>{messages.cantFindPatient}</Typography>
+              <Button variant="outlined" onClick={() => setManualEntry(true)}>
+                {messages.addManualEntry}
+              </Button>
+            </Container>
+          </>
         )}
       </Paper>
     </StepperContainer>
