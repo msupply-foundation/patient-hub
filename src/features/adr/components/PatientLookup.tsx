eabb1c7072f1320fc938277639fae9809a2084d0
Mark Prins <mark@malcolmprins.net>
2021-06-16 15:13:09 +1200

    handle auth timeout

--- src/features/adr/components/PatientLookup.tsx
+++ src/features/adr/components/PatientLookup.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC, useMemo, useState } from "react";
+import { ChangeEvent, FC, useMemo, useState, useEffect } from "react";
 import {
   Box,
   Button,
@@ -18,8 +18,12 @@ import { PatientList } from "../components/PatientList";
 import { stylesFactory } from "../../../shared/utils";
 import { useEnterHandler, useTranslations } from "../../../shared/hooks";
 import { usePatientLookup } from "../../patients/hooks";
+import { useAuth } from "../../auth/hooks";
 import { isValid, parse } from "date-fns";
 import { Patient, SearchParameters } from "../../patients/types";
+
+import { useModal } from "../../../shared/hooks";
+import { ModalKey } from "../../../shared/containers/ModalProvider";
 
 const useStyles = stylesFactory({
   loadingIndicator: { textAlign: "center", padding: 15 },
@@ -62,6 +66,8 @@ export const PatientLookup: FC<PatientLookupProps> = ({
   };
 
   const {
+    authenticated,
+    authenticationRequired,
     data: patientData,
     error,
     noMore,
@@ -96,6 +102,25 @@ export const PatientLookup: FC<PatientLookupProps> = ({
   );
 
   useEnterHandler(lookupPatients);
+  const { isOpen, open } = useModal(ModalKey.login);
+  const { isGuest, username } = useAuth();
+
+  // if authentication is required, show login modal
+  useEffect(() => {
+    if (authenticationRequired) {
+      open();
+    } 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [authenticationRequired, username]);
+
+  // if currently searching and the modal state changes and we have a valid user
+  // search again
+  useEffect(() => {
+    if(username && !isOpen && !isGuest && loading) {
+      authenticated();
+      searchOnline(searchParams);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }  }, [isOpen])
 
   return (
     <Grid container direction="row" justifyContent="space-around">
